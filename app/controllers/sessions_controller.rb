5fb15d22e1ff5bf0810781e3d14a532aec146db9
MariaTikhonova <ritz.rkraft@gmail.com>
2019-02-01 14:49:48 +0200

    mysql2 for production

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SessionsController < Devise::SessionsController 
+class SessionsController < Devise::SessionsController 
 
   def create
     user = User.find_by_email(sign_in_params[:email])
